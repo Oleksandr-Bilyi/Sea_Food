41e38b39cda5df235ae870f8741b363d78755b99
Oleksandr-Bilyi <126759417+Oleksandr-Bilyi@users.noreply.github.com>
2023-12-06 18:14:19 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -63,6 +63,7 @@ if(iconMenu) {
 
 //Прокрутка при Нажиманні
 
+/*
 const menulinks = document.querySelectorAll('.menu__link[data-goto]');
 
 if(menulinks.length > 0) {
@@ -84,3 +85,110 @@ if(menulinks.length > 0) {
 		}
 	}
 }
+*/
+
+//Анімація Пупапів
+/*
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if(popupLinks.length > 0) {
+	for(let index = 0; index < popupLinks.length; index++){
+		const popupLink = popupLinks[index];
+		popupLink.addEventListener("click", function (e){
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const currentPopup = document.getElementById(popupName);
+			popupOpen(currentPopup);
+			e.preventDefault();
+		});
+	}
+}
+//Close-icon
+const popupCloseIcon = document.querySelectorAll('close-popup');
+if(popupCloseIcon.length > 0){
+	for(let index; index < popupCloseIcon.length; index++){
+		const el =popupCloseIcon[index];
+		el.addEventListener("click", function (e) {
+			popupClose(el.closest('.popup'));
+			e.preventDefault();
+		});
+	}
+}
+//Open
+function popupOpen(currentPopup) {
+	if(currentPopup && unlock) {
+		const popupActive = document.querySelector('.popup.open');
+		if (popupActive) {
+			popupClose(popupActive, false);
+		} 	else {
+			bodyLock();
+		}
+		currentPopup.classList.add('open');
+		currentPopup.addEventListener("click", function(e){
+			if (!e.target.closest('.popup__content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+
+		});
+	}
+}
+//Close popup
+function popupClose(popupActive, doUnlock = true) {
+	if (unlock) {
+		popupActive.classList.remove('open');
+		if (doUnlock) {
+			bodyUnlock();
+		}
+
+	}
+}
+//For overflow
+function bodyLock() {
+	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+	if(lockPadding.length > 0){
+		for (let index = 0; index < lockPadding.length; index++){
+			const el = lockPadding[index];
+			el.style.paddingRight = lockPaddingValue;
+		}
+	}
+	body.style.paddingRight = lockPaddingValue;
+	body.classList.add('lock');
+
+	unlock = false;
+	setTimeout(function (){
+		unlock = true;
+	}, timeout);
+}
+
+function bodyUnlock() {
+	setTimeout(function(){
+		if(lockPadding.length > 0){
+			for (let index = 0; index < lockPadding.length; index++){
+				const el = lockPadding[index];
+				el.style.paddingRight = '0px'
+			}
+		}
+		body.style.paddingRight = '0px';
+		body.classList.remove('lock');
+	}, timeout);
+
+	unlock = false;
+	setTimeout(function(){
+		unlock = true;
+	}, timeout);
+}
+*/
+
+/*
+document.addEventListener('keydown', function (e){
+	if (KeyboardEvent(e) == 27) {
+		const popupActive = document.querySelector('.popup.open');
+		popupClose(popupActive);
+	}
+});
+*/
